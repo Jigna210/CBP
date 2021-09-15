d4506c21f5a426346029faa2370ed8d647cbb386
Mahant <chavdajigna985@gmail.com>
2021-09-15 21:19:36 +0530

    Added chrome for Linux

--- CrossBorderPickups/cross_border/lib/configs/config.py
+++ CrossBorderPickups/cross_border/lib/configs/config.py
@@ -6,7 +6,7 @@ class Config(object):
     """ Configuration variables related to automation framework """
 
     PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    CHROME_DRIVER_DIR = os.path.join(PROJECT_ROOT_DIR, os.path.join("drivers", "chrome_driver", "chromedriver.exe"))
+    CHROME_DRIVER_DIR = os.path.join(PROJECT_ROOT_DIR, os.path.join("drivers", "chrome_driver", "chromedriver"))
     FIREFOX_DRIVER_DIR = os.path.join(PROJECT_ROOT_DIR, os.path.join("drivers", "firefox_driver", "geckodriver.exe"))
     PLATFORM_NAME = platform.system()
     SCREENSHOTS_DIR = os.path.join(PROJECT_ROOT_DIR, os.path.join("output", "screenshots"))
