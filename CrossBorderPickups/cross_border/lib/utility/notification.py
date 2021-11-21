813185a81bd5374931ae47966f41edcaeb90f570
Jigna Chavda <chavdajigna985@gmail.com>
2021-11-21 17:47:00 +0000

    notification.py Notification_message

--- CrossBorderPickups/cross_border/lib/utility/notification.py
+++ CrossBorderPickups/cross_border/lib/utility/notification.py
@@ -6,17 +6,19 @@ class Notifications(BasePage):
     """ Defines functions to retrieve the success notification message """
 
     @property
-    def success_message(self) -> str:
+    def notification_message(self) -> str:
         """
         Returns success notification message
 
         :return: notification message
         :rtype: str
         """
-        self.wait_for_element(lambda: self.is_element_visible(by_locator=Locators.notification_msg_div),
+        expected_locator = Locators.ops_notification_msg_text if "-ops-" in self.get_url() else \
+            Locators.notification_msg_text
+        self.wait_for_element(lambda: self.is_element_visible(by_locator=expected_locator),
                               waiting_for="notification message gets populated")
 
-        return self.get_element_text(by_locator=Locators.notification_msg_text)
+        return self.get_element_text(by_locator=expected_locator)
 
     def get_notification_message(self) -> str:
         """
